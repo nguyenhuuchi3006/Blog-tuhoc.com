248ef4ba71dca1e34a088906588a8ba38e00c93f
Chi Barca <nguyenhuuchi3006@gmail.com>
2019-03-10 22:53:08 +0700

    code fe header, footer ejs layout

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const bodyParser = require('body-parser');
 
+app.set('view engine', 'ejs');
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({
+    extended: true
+  }));
 
+app.get('/', (req, res) => {
+    res.render("index");
+});
 
-app.get('/', (req, res) => res.send('Hello World!'));
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => 
+    console.log(`Server is listening on port ${port}!`)
+);

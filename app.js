68b066f7fc94ab88a69433d7a5ee3d22afa290cb
Chi Barca <nguyenhuuchi3006@gmail.com>
2019-03-12 16:17:46 +0700

    add config mongoose, code fe page post, connect home with post

--- app.js
+++ app.js
@@ -2,17 +2,64 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+var _ = require('lodash');
+
 
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({
     extended: true
-  }));
+}));
 
-app.get('/', (req, res) => {
-    res.render("index");
+
+// ----------install database-----
+mongoose.connect('mongodb://localhost:27017/blog-tuhoc', {
+    useNewUrlParser: true
 });
 
-app.listen(port, () => 
+const postSimple = {
+    title: String,
+    content: String
+}
+
+const PostSimple = mongoose.model("PostSimple", postSimple);
+
+
+
+
+app.get('/', (req, res) => {
+    const test = _.kebabCase('Điều gì xảy ra khi gõ một đường dẫn lên browser');
+
+    res.render("index", {
+        test: test
+    });
+});
+
+
+app.get('/post', (req,res)=>{
+    res.render("post");
+});
+
+app.get('/post/:postName', (req,res) => {
+    const postName = req.params.postName;
+    //_.kebabCase(postName)
+    res.render("post");
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(port, () =>
     console.log(`Server is listening on port ${port}!`)
 );
